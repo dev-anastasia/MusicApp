86c65d4cd902c9c8265a04df3140032b9a822291
Anastasia Stepanova <stepanovawm@gmail.com>
2023-09-13 20:05:02 +0300

    Init

---
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.musicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
